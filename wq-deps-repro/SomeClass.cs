ef18dbc78e7ac8089ef7fab4ab2425f895699daa
siim talvik <siim@aiotex.com>
2018-02-19 11:09:30 +0200

    public reproduction project

---
+++ wq-deps-repro/SomeClass.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RabbitMQ.Client;
+
+namespace wq_deps_repro
+{
+    public class SomeClass
+    {
+        public ConnectionFactory _factory;
+
+
+        public SomeClass()
+        {
+
+        }
+
+        public string SomeFunctionReturningString()
+        {
+            this._factory = new ConnectionFactory()
+            {
+                UserName = "",
+                Password = "",
+                VirtualHost = "/",
+                HostName = "",
+                Port = 1235
+            };
+
+            return "just something returned";
+        }
+    }
+}
---
+++ wq-deps-repro/hidden/CantTouchThis.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace wq_deps_repro.hidden
+{
+    public class CantTouchThis
+    {
+        public CantTouchThis()
+        {
+
+        }
+    }
+}
